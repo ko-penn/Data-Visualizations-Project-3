4e0a0773928e29bbbdfb3ca16b9b40696a97ea5a
Devin Harris <devinharris.316@gmail.com>
2024-04-17 20:45:20 -0400

    added total count badges to episodes and characters selections

--- js/helpers/episode-form-builder.js
+++ js/helpers/episode-form-builder.js
@@ -21,13 +21,62 @@ export class EpisodeFormBuilder {
 
    handleChange() {
       handleGlobalFilterChange();
+
+      const totalSeasonCounts = {};
+      const totalSelectedSeasonsCounts = {};
+      rawData.forEach((d) => {
+         const { season, episode } = d;
+         if (!totalSelectedSeasonsCounts[season]) {
+            totalSelectedSeasonsCounts[season] = new Set();
+         }
+         if (!totalSeasonCounts[season]) {
+            totalSeasonCounts[season] = new Set();
+         }
+         totalSeasonCounts[season].add(episode);
+      });
+
+      this.checkboxs.forEach((c) => {
+         if (c.checked) {
+            const season = c.getAttribute('data-season');
+            const episode = c.getAttribute('data-episode');
+            if (!totalSelectedSeasonsCounts[season]) {
+               totalSelectedSeasonsCounts[season] = new Set();
+            }
+            totalSelectedSeasonsCounts[season].add(episode);
+         }
+      });
+
+      let totalCount = 0;
+      Object.keys(totalSelectedSeasonsCounts).forEach((k) => {
+         const item = this.seasonItemPills[k];
+         const seasonCount = totalSelectedSeasonsCounts[k].size;
+         item.innerText = seasonCount;
+         totalCount += seasonCount;
+         if (totalSeasonCounts[k].size === seasonCount) {
+            item.variant = 'success';
+         } else {
+            item.variant = 'neutral';
+         }
+      });
+
+      this.tooltipInfoBadge.innerText = totalCount;
    }
 
    buildEpisodesTree() {
       const h3 = document.createElement('h3');
       h3.style.padding = '.5em';
-      h3.innerText = 'Episode/Season';
-      this.form.append(h3);
+      h3.innerText = 'Episodes';
+      this.buildTooltip();
+
+      const header = document.createElement('div');
+      header.append(h3);
+      header.append(this.episodesTotalTooltip);
+      header.style.display = 'flex';
+      header.style['justifyContent'] = 'space-between';
+      header.style['alignItems'] = 'center';
+      header.style.width = '100%';
+      this.form.append(header);
+
       const tree = document.createElement('sl-tree');
       const seasons = {};
       rawData.forEach((d) => {
@@ -52,6 +101,8 @@ export class EpisodeFormBuilder {
             const episodeTreeItem = document.createElement('sl-tree-item');
             const episodeItemCheckbox = document.createElement('sl-checkbox');
             episodeItemCheckbox.setAttribute('data-key', `${k}-${e}`);
+            episodeItemCheckbox.setAttribute('data-season', k);
+            episodeItemCheckbox.setAttribute('data-episode', e);
             episodeItemCheckbox.innerText = `Episode ${e}: ${seasons[k][e].title}`;
             episodeItemCheckbox.checked = true;
             this.checkboxs.push(episodeItemCheckbox);
@@ -86,5 +137,44 @@ export class EpisodeFormBuilder {
       });
 
       this.form.append(tree);
+   }
+
+   buildTooltip() {
+      this.episodesTotalTooltip = document.createElement('sl-tooltip');
+
+      this.tooltipInfoBadge = document.createElement('sl-badge');
+      this.tooltipInfoBadge.variant = 'primary';
+      this.tooltipInfoBadge.pill = true;
+      this.episodesTotalTooltip.append(this.tooltipInfoBadge);
+
+      this.episodesTotalTooltipContent = document.createElement('div');
+      this.episodesTotalTooltipContent.slot = 'content';
+
+      this.episodesTotalTooltipMenu = document.createElement('sl-menu');
+      const label = document.createElement('sl-menu-label');
+      label.innerText = 'Total Episodes Selected';
+      this.episodesTotalTooltipMenu.append(label);
+
+      const seasons = rawData.reduce((acc, curr) => {
+         acc.add(curr.season);
+         return acc;
+      }, new Set());
+
+      this.seasonItemPills = {};
+
+      for (let season of seasons) {
+         const seasonItem = document.createElement('sl-menu-item');
+         seasonItem.innerText = `Season ${season}`;
+         const seasonItemPill = document.createElement('sl-badge');
+         seasonItemPill.variant = 'neutral';
+         seasonItemPill.slot = 'suffix';
+         seasonItemPill.pill = true;
+         seasonItem.append(seasonItemPill);
+         this.seasonItemPills[season] = seasonItemPill;
+         this.episodesTotalTooltipMenu.append(seasonItem);
+      }
+
+      this.episodesTotalTooltipContent.append(this.episodesTotalTooltipMenu);
+      this.episodesTotalTooltip.append(this.episodesTotalTooltipContent);
    }
 }
